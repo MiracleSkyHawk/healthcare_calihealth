083546bcf1b8fa06479d754171aba729bb91d4ed
zechen.ren@mail.mcgill.ca <time6098@gmail.com>
2023-02-09 17:44:40 -0500

    Coverage of CaliHealth

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import numpy as np
 caliHealthAreasFile = open("CaliHealthCounties.txt", mode='r')
 caliHealthAreasList = caliHealthAreasFile.read().splitlines()[1:]
 caliHealthAreas = pd.DataFrame({'serviceAreas': caliHealthAreasList})
-
-countiesList = pd.read_csv('CaliforniaZipCodeCounties.csv')
 caliHealthAreas.to_csv('caliHealthAreas.csv')
 
+countiesList = pd.read_csv('CaliforniaZipCodeCounties.csv')
+censusData = pd.read_csv('CaliforniaCensusDataByZip.csv')
 
+df_caliHealthCoverage = pd.merge(caliHealthAreas, countiesList, how='inner', left_on='serviceAreas', right_on='county')
+df_caliHealthCoverage = pd.merge(df_caliHealthCoverage, censusData, how='inner', left_on='zip', right_on='Geographic Area Name')
 
-
-
+df_caliHealthCoverage.to_csv(r'Coverage of CaliHealth.csv', index=False)
 
 
 
