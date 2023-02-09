3b4e41c76bbc88a2de4bdd7bed3e9e677e0eca73
MapleKnightOfRiverwood <davidliu1007@gmail.com>
2023-02-09 16:41:43 -0500

    first commit

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+caliHealthAreasFile = open("CaliHealthCounties.txt", mode='r')
+caliHealthAreasList = caliHealthAreasFile.read().splitlines()[1:]
+caliHealthAreas = pd.DataFrame({'serviceAreas': caliHealthAreasList})
+
+countiesList = pd.read_csv('CaliforniaZipCodeCounties.csv')
+countiesList.to_csv('caliHealthAreas.csv')
+
 
 
 
